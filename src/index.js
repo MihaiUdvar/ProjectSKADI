a0119f8bfa137062de0d11fa09f75a0d1549c844
“Mihai <freeride_mihai@yahoo.com>
2021-04-21 14:08:04 +0200

    sign in buttons updated

--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ import { ui } from "./ui.js";
 
 document.addEventListener("DOMContentLoaded", ui.updateNavCartIcon());
 
-let adminLogin = document.getElementById("admin-login");
+// Faking customer and admin passwords
+// let adminLogin = document.getElementById("admin-login");
 
-adminLogin.addEventListener("click", function (e) {
-  let password = document.getElementById("password").value;
-  let username = document.getElementById("username").value;
-  if (username === "admin" && password === "1234") {
-    window.location.href = "admin.html";
-  }
-});
+// adminLogin.addEventListener("click", function (e) {
+//   let password = document.getElementById("password").value;
+//   let username = document.getElementById("username").value;
+//   if (username === "admin" && password === "1234") {
+//     window.location.href = "admin.html";
+//   }
+// });
 
-let customerLogin = document.getElementById("customer-login");
+// let customerLogin = document.getElementById("customer-login");
 
-customerLogin.addEventListener("click", function (e) {
-  let password = document.getElementById("password").value;
-  let username = document.getElementById("username").value;
-  if (username === "customer" && password === "1234") {
-    window.location.href = "products_all.html";
-  }
-});
+// customerLogin.addEventListener("click", function (e) {
+//   let password = document.getElementById("password").value;
+//   let username = document.getElementById("username").value;
+//   if (username === "customer" && password === "1234") {
+//     window.location.href = "products_all.html";
+//   }
+// });
